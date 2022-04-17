42588343b241347d50609255039bc1d76435928f
deepakmeena635 <37202245+deepakmeena635@users.noreply.github.com>
2022-04-17 23:44:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,38 +1,23 @@
-class drop{
-  
-   constructor(){
-      this.x = random(0, windowWidth )
-      this.y =0
-     this.speed= 50; 
-     this.dropSize = 5; 
-    } 
-    reset(){
-      this.x = random(0, windowWidth )
-      this.y =0
-     this.speed= 10; 
-     this.dropSize = 5; 
-    } 
-    
-   fall(speed =0 ){
-     this.y =this.y+ this.speed
-     // console.log(this.y)
-      if (speed !=0 ){
-        this.speed = speed
-      }
-     if (this.y <= height && this.y > height- 50  ){
-        // console.log(".")
-        var myDegrees = int(15+ 30/(this.y-15))
-        let la = p5.Vector.fromAngle(radians(myDegrees), this.dropSize);
-        let ra = p5.Vector.fromAngle(radians(myDegrees), this.dropSize);
-        line(this.x , this.y+this.dropSize, this.x-la.x, this.y-la.y)
-        line(this.x , this.y+this.dropSize, this.x+ra.x, this.y-ra.y)
-        line(this.x, this.y , this.x, this.y+this.dropSize )
-        this.reset();
-      }
-      line(this.x, this.y , this.x, this.y+this.dropSize )
-      stroke(255)
-      
+nbDrops = 600
+var arr = [] ; 
+
+function setup() {
+  createCanvas(600, 600);
+  for(var i =0 ; i< nbDrops ; i++){
+     arr[i] = new  drop();
   }
   
 }
 
+function draw() {
+   background(50)
+
+//   circle(0, height, 30)
+  // fill(255)
+  // stroke(255)
+    for(var i =0 ; i< nbDrops ; i++){
+      stroke(noise(i*i,i,sin(i))*255, noise(i*i,2*i,cos(i))*255,noise(i,i*i, i*i)*255)
+     arr[i].fall(random(0,1)*30);
+    }
+  // background(10)
+}
