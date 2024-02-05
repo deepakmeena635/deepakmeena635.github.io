d634e6b3ada6a14bffbda3b9b290691e38c0d85b
DEEPAK MEENA <37202245+deepakmeena635@users.noreply.github.com>
2024-02-05 21:26:35 +0530

    nothing

--- sketch.js
+++ sketch.js
@@ -10,14 +10,10 @@ function setup() {
 }
 
 function draw() {
-   background(50)
+   background(50, 50,50, 51)
 
-//   circle(0, height, 30)
-  // fill(255)
-  // stroke(255)
     for(var i =0 ; i< nbDrops ; i++){
       stroke(noise(i*i,i,sin(i))*255, noise(i*i,2*i,cos(i))*255,noise(i,i*i, i*i)*255)
      arr[i].fall(random(0,1)*30);
     }
-  // background(10)
 }
